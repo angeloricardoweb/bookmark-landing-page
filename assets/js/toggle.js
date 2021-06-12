20f5017cedda7674b20ed87201c682e84d627c49
Angelo Ricardo <angeloengcomp@gmail.com>
2021-06-12 16:29:41 -0300

    att toggle

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,9 +1,34 @@
 const btnMobile = document.getElementById('btn-mobile');
+// const features = document.getElementById('btn-features');
+// const contact = document.getElementById('btn-contact');
+// const pricing = document.getElementById('btn-pricing');
+// const login = document.getElementById('btn-login');
+const nav = document.getElementById('nav');
 
 function toggleMenu() {
     console.log('click');
-    const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
+
+document.addEventListener('click', function (e) {
+    const elemento = e.target;
+    if (elemento.classList.contains('btn-features')) {
+        toggleMenu()
+    }
+    if (elemento.classList.contains('btn-pricing')) {
+        toggleMenu()
+    }
+    if (elemento.classList.contains('btn-contact')) {
+        toggleMenu()
+    }
+    if (elemento.classList.contains('btn-login')) {
+        toggleMenu()
+    }
+});
+
+// features.addEventListener('click', toggleMenu);
+// pricing.addEventListener('click', toggleMenu);
+// contact.addEventListener('click', toggleMenu);
+// login.addEventListener('click', toggleMenu);
 
 btnMobile.addEventListener('click', toggleMenu);
